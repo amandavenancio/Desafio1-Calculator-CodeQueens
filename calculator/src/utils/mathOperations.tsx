5215e9f88d92b21167f81b0ba2da3a85808a6e70
Amanda Venancio <aalmeidaven@gmail.com>
2025-03-24 21:48:26 -0300

    atualiza mathOperations

--- calculator/src/utils/mathOperations.tsx
+++ calculator/src/utils/mathOperations.tsx
@@ -4,5 +4,6 @@ export const multiply = (a: number, b: number) => formatNumber(a * b);
 export const divide = (a: number, b: number) => (b !== 0 ? formatNumber(a / b) : "Erro (divisão por zero)");
 
 const formatNumber = (num: number): number => {
-  return Number.isInteger(num) ? num : parseFloat(num.toFixed(2));
+  const formatted = num.toFixed(2); // Arredonda para 2 casas decimais
+  return parseFloat(formatted); // Retorna como número
 };
